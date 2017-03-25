8a42ce5e9fa532ecbcb561846a0f4c0c64df46d3
Nathan Hill <nathan.hill@inin.com>
2017-03-24 20:10:15 -0400

    Updated the instrument component so that the image URL is passed in.

--- app/components/instrument/instrument.js
+++ app/components/instrument/instrument.js
@@ -2,6 +2,10 @@ import Component from 'nativescript-component';
 
 class Instrument extends Component {
 
+    onLoaded() {
+        console.log('onLoaded was invoked!');
+        super.onLoaded(...arguments);
+    }
 }
 
 Instrument.export(exports);
