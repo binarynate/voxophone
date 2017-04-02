6ff3630771abbcac8663b45e1608c6dea811b72b
Nathan Hill <nathan.hill@inin.com>
2017-04-02 14:17:34 -0400

    Updated the add-instruments script to also add the soundbank file and the piano instrument so the instruments can be rebuilt from scratch every time.

---
+++ scripts/add-instruments.js
@@ -0,0 +1,61 @@
+import path from 'path';
+import logger from 'winston';
+import InstrumentManager from '../app/models/InstrumentManager';
+import NodeFileSystemJsonStorage from './util/NodeFileSystemJsonStorage';
+import NodeFileInfoStorage from './util/NodeFileInfoStorage';
+
+const storageBasePath = __dirname + '/../app/data';
+
+let defaultSoundBank = {
+    'name': 'GeneralUser GS MuseScore v1.442.sf2'
+};
+defaultSoundBank.filePath = path.join(__dirname, 'files', defaultSoundBank.name)
+
+let instruments = [
+    {
+        'name': 'Acoustic Grand Piano',
+        'soundBankProgramNumber': 0,
+        'imageInfo': {
+            name: 'acoustic-grand-piano.png'
+        }
+    },
+    {
+        'name': 'Alto Sax',
+        'soundBankProgramNumber': 65,
+        'imageInfo': {
+            name: 'alto-sax.jpg'
+        }
+    }
+];
+
+let instrumentStorage = new NodeFileSystemJsonStorage({
+    directoryPath: `${storageBasePath}/instruments`
+});
+
+let fileInfoStorage = new NodeFileInfoStorage({
+    directoryPath: `${storageBasePath}/fileInfo`
+});
+
+let instrumentManager = new InstrumentManager({
+    logger,
+    instrumentStorage,
+    fileInfoStorage
+});
+
+logger.info('Saving the instruments...');
+
+fileInfoStorage.insert(defaultSoundBank)
+.then(soundBankInfo => {
+
+    // Add the sound bank and the imageInfo file path for each instrument
+    for (let instrument of instruments) {
+
+        Object.assign(instrument, { soundBankInfo });
+        let { imageInfo } = instrument;
+        imageInfo.filePath = path.join(__dirname, 'files', imageInfo.name);
+    }
+
+    return instrumentManager.saveInstruments({ instruments });
+})
+.then(() => logger.info('Successfully finished saving the instruments!'))
+.catch(error => logger.error(`An error occurred while saving the instruments: ${error.stack}`));
