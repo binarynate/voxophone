624f3f94ef0f8b774084914ca5f001c5fb2d109d
Nathan Hill <nathan.hill@inin.com>
2017-04-03 02:36:29 -0400

    checkpoint on cleaning up the code in music-note-meter.

--- app/components/music-note-meter/music-note-meter.js
+++ app/components/music-note-meter/music-note-meter.js
@@ -4,6 +4,8 @@ import ColorEditor from '../../utils/Color';
 import Component from 'nativescript-component';
 import delay from '../../utils/delay';
 
+const NUMBER_OF_RINGS = 4;
+
 class MusicNoteMeter extends Component {
 
     /**
@@ -21,54 +23,45 @@ class MusicNoteMeter extends Component {
                 // The view hasn't been fully initialized. Let's try again in 1 millisecond.
                 return delay(1).then(() => this.init());
             }
-            this.view.height = this.view.width = outerRingRadius * 2;
-            this.view.borderRadius = outerRingRadius;
+            // Set the height and width equal to each other, because this view is initially just a rectangle.
+            outerRing.height = this.view.width = outerRingRadius * 2;
+            outerRing.borderRadius = outerRingRadius;
 
 
             let center = this.view.getViewById('meter-center');
             this.view.removeChild(center);
-            let centerSize = center.getActualSize();
-            let centerRadius = centerSize.width / 2;
 
-            console.log('Generating rings...');
+            let radiusLengthToDivide = outerRing.borderRadius - center.borderRadius;
+            let radiusStepSize = radiusLengthToDivide / NUMBER_OF_RINGS;
 
-            let numberOfRings = 4;
-            let radiusLengthToDivide = outerRingRadius - centerRadius;
-            let radiusStepSize = radiusLengthToDivide / numberOfRings;
-
-            let rings = [];
-            let previousRing = center;
-
-            for (let i = 0; i < numberOfRings - 1; i++) {
-
-                let radius = previousRing.borderRadius + radiusStepSize;
-                let ring = this._createRing(radius);
-                rings.push(ring);
-                previousRing = ring;
-            }
+            let numberOfRingsToCreate = NUMBER_OF_RINGS - 1; // Subtract one, since the outer ring already exists.
+            let rings = this._createRings(center, numberOfRingsToCreate, radiusStepSize);
             rings.push(outerRing);
 
+            // Link the rings together in a parent-child hierarchy.
+            this._linkRingsTogether(rings);
             rings[0].addChild(center);
-            rings.forEach((ring, index) => {
-                let parentRing = rings[index + 1];
 
-                if (parentRing) {
-                    parentRing.addChild(ring);
-                }
-            });
+            this._setRingColors(rings);
 
-            let rootColor = '#ff871e';
-
-            rings.forEach((ring, index) => {
-
-                let { r, g, b } = ColorEditor(rootColor).lighten(index * 0.2).rgb().object();
-                ring.backgroundColor = new Color(255, r, g, b);
-            });
-
-            console.log('made it to the end of initialization!');
         } catch (error) {
             console.log(`An error occurred during ring creation. ${error.message}: ${error.stack}`);
         }
+    }
+
+    _createRings(initialRing, numberOfRings, radiusStepSize) {
+
+        let previousRing = initialRing,
+            rings = [];
+
+        for (let i = 0; i < numberOfRings; i++) {
+
+            let radius = previousRing.borderRadius + radiusStepSize;
+            let ring = this._createRing(radius);
+            rings.push(ring);
+            previousRing = ring;
+        }
+        return rings;
     }
 
     _createRing(radius) {
@@ -77,7 +70,6 @@ class MusicNoteMeter extends Component {
         ring.height = ring.width = radius * 2;
         ring.borderRadius = radius;
         let color = this._getRandomColor();
-        console.log('color: ' + color);
         ring.backgroundColor = new Color(color);
         ring.alignItems = AlignItems.CENTER;
         ring.justifyContent = JustifyContent.CENTER;
@@ -90,6 +82,28 @@ class MusicNoteMeter extends Component {
 
         let num = Math.floor(Math.random() * 0xFFFFFF);
         return '#' + num.toString(16);
+    }
+
+    _linkRingsTogether(rings) {
+
+        rings.forEach((ring, index) => {
+            let parentRing = rings[index + 1];
+
+            if (parentRing) {
+                parentRing.addChild(ring);
+            }
+        });
+    }
+
+    _setRingColors(rings) {
+
+        let rootColor = '#ff871e';
+
+        rings.forEach((ring, index) => {
+
+            let { r, g, b } = ColorEditor(rootColor).lighten(index * 0.2).rgb().object();
+            ring.backgroundColor = new Color(255, r, g, b);
+        });
     }
 }
 
